66e24f28d3ade84510185f16b6678c4b87231dee
BG <Bgee269@hotmail.com>
2019-02-28 22:42:04 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -6,20 +6,20 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
   } else {
     connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
+      host: '	tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      user: '	dnbwomrxgyrvver2',
       password: 'Charmgizmo3!',
       database: 'burger2_db'
     });
   };
 
-//var connection = mysql.createConnection({
- // host: "localhost",
- // port: 3306,
-  //user: "root",
- // password: "Charmgizmo3!",
- // database: "burger2_db"
-//});
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Charmgizmo3!",
+  database: "burger2_db"
+});
 
 // Make connection.
 connection.connect(function(err) {
